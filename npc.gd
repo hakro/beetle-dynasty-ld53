f517e0de430e58384aa417d395e15953da39b260
Hakim <hakim.rouatbi@gmail.com>
2023-04-30 14:46:18 +0200

    tween dialog bubble, add new font

--- npc.gd
+++ npc.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
-
-@onready
-var interct_area : Area2D = $InteractionArea
-
-@onready
-var dialog_bubble : Node2D = $DialogBubble
+@onready var interct_area : Area2D = $InteractionArea
+@onready var dialog_bubble : Node2D = $DialogBubble
 
 func _ready() -> void:
 	# Player gets here
@@ -14,7 +10,12 @@ func _ready() -> void:
 	interct_area.connect("body_exited", _hide_dialog_bubble)
 
 func _show_dialog_bubble(_player: Node2D) -> void:
+	dialog_bubble.scale = Vector2.ZERO
 	dialog_bubble.show()
+	var tween = create_tween()
+	tween.tween_property(dialog_bubble, "scale", Vector2(1,1), 0.5).set_trans(Tween.TRANS_ELASTIC)
 
 func _hide_dialog_bubble(_player: Node2D) -> void:
-	dialog_bubble.hide()
+	var tween = create_tween()
+	tween.tween_property(dialog_bubble, "scale", Vector2(0,0), 0.5).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_callback(dialog_bubble.hide)
