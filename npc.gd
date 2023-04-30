2631e950761aff16059b30836e9aa37ae9e6d666
Hakim <hakim.rouatbi@gmail.com>
2023-05-01 00:26:23 +0200

    Limit wall & Last basic gameplay

--- npc.gd
+++ npc.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var interct_area : Area2D = $InteractionArea
 @onready var dialog_bubble : Node2D = $DialogBubble
 @onready var dialog_label : Label = $DialogBubble/Label
+@onready var limit_wall : StaticBody2D = $LimitWall
 
 func _ready() -> void:
 	# Player gets here
@@ -18,11 +19,29 @@ func _show_dialog_bubble(_player: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property(dialog_bubble, "scale", Vector2(1,1), 0.5).set_trans(Tween.TRANS_ELASTIC)
 	set_bubble_text()
+	
+	# Lock until mission activated
+	if GameManager.current_step == GameManager.Steps.DELIVER_FLOWERS_PENDING or \
+		GameManager.current_step == GameManager.Steps.DELIVER_MUSHROOMS_PENDING:
+		limit_wall.process_mode = Node.PROCESS_MODE_INHERIT
+		limit_wall.show()
+	else:
+		limit_wall.process_mode = Node.PROCESS_MODE_DISABLED
+		limit_wall.hide()
 
 func _hide_dialog_bubble(_player: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property(dialog_bubble, "scale", Vector2(0,0), 0.5).set_trans(Tween.TRANS_ELASTIC)
 	tween.tween_callback(dialog_bubble.hide)
+	
+	# Lock until mission activated
+	if GameManager.current_step == GameManager.Steps.DELIVER_FLOWERS_PENDING or \
+		GameManager.current_step == GameManager.Steps.DELIVER_MUSHROOMS_PENDING:
+		limit_wall.process_mode = Node.PROCESS_MODE_INHERIT
+		limit_wall.show()
+	else:
+		limit_wall.process_mode = Node.PROCESS_MODE_DISABLED
+		limit_wall.hide()
 
 # This function shows text in the dialog bubble depending on the situation
 func set_bubble_text() -> void:
